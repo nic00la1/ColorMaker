ca07bbe21be770fbde4d27b08949beebe22f33cb
nic00la1 <nic00la.kaleta@gmail.com>
2023-10-05 20:11:33 +0200

    Add project files.

---
+++ ColorMaker/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ColorMaker
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
