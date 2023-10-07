8d499e149a9d91fea11f9e86ca1bbe4832b9a154
nic00la1 <nic00la.kaleta@gmail.com>
2023-10-07 13:59:56 +0200

    Changing the background color works ✅

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -8,6 +8,22 @@
             InitializeComponent();
         }
 
-      
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
+
+            Color color = Color.FromRgb(red, green, blue);
+
+            SetColor(color);
+        }
+
+        private void SetColor(Color color)
+        {
+            btnRandom.BackgroundColor = color;
+            Container.BackgroundColor = color;
+            lblHex.Text = color.ToHex();
+        }
     }
 }
